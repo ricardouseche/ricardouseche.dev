827a4937eb375286469bd593404c1884258817e6
Ricardo Useche <riuseche24@gmail.com>
2020-08-18 23:01:55 -0500

    added some js

---
+++ randomHeader.js
@@ -0,0 +1,18 @@
+let candidates = [
+  'Just some thoughts.',
+  "Man, that's hot.",
+  "Didn't know that.",
+  'Okay then.',
+];
+
+function getPhrase(phrases) {
+  let header = document.getElementById('quote');
+  picked = phrases[Math.floor(Math.random() * phrases.length)];
+  console.log(Math.floor(Math.random() * phrases.length));
+  console.log(picked);
+  header.innerHTML = picked;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  getPhrase(candidates);
+});
